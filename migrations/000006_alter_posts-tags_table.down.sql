bfbad2e775e591261f4ec10c72db180d8ad89b1c
kmj36 <kmj36953695@gmail.com>
2026-02-20 19:14:42 +0900

    [ADD] create table index

---
+++ migrations/000006_alter_posts-tags_table.down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE posts_tags RENAME COLUMN post_id TO postid;
+ALTER TABLE posts_tags RENAME COLUMN tag_id TO tagid;
---
+++ migrations/000006_alter_posts-tags_table.up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE posts_tags RENAME COLUMN postId TO post_id;
+ALTER TABLE posts_tags RENAME COLUMN tagId TO tag_id;
