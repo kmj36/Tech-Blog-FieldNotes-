b838e5f560b5044dc52055bcad7612b2ddd20ad6
kmj36 <kmj36953695@gmail.com>
2026-03-06 22:05:37 +0900

    [REFACTOR] add db struct tags to Post model for DB field mapping

--- backend/internal/model/post.go
+++ backend/internal/model/post.go
@@ -6,18 +6,18 @@ import (
 
 // 도메인 엔티티 (API <-> DB)
 type Post struct {
-	ID				int32		`json:"id"`
-	AccountID 		string		`json:"account_id"`
-	Slug			string		`json:"slug"`
-	Title			string		`json:"title"`
-	Content			string		`json:"content"`
-	Thumbnail		string		`json:"thumbnail"`
-	CategoryID		int16		`json:"category_id"`
-	PublishedAt		time.Time	`json:"published_at"`
-	IsPrivate		bool		`json:"is_private"`
-	CreatedAt		time.Time	`json:"created_at"`
-	UpdatedAt		time.Time	`json:"updated_at"`
-	DeletedAt		time.Time	`json:"deleted_at"`
+	ID				int32		`json:"id" db:"id"`
+	AccountID 		string		`json:"account_id" db:"account_id"`
+	Slug			string		`json:"slug" db:"slug"`
+	Title			string		`json:"title" db:"title"`
+	Content			string		`json:"content" db:"content"`
+	Thumbnail		string		`json:"thumbnail" db:"thumbnail"`
+	CategoryID		int16		`json:"category_id" db:"category_id"`
+	PublishedAt		time.Time	`json:"published_at" db:"published_at"`
+	IsPrivate		bool		`json:"is_private" db:"is_private"`
+	CreatedAt		time.Time	`json:"created_at" db:"created_at"`
+	UpdatedAt		time.Time	`json:"updated_at" db:"updated_at"`
+	DeletedAt		time.Time	`json:"deleted_at" db:"deleted_at"`
 }
 
 // 요청 DTO (Client -> API)
